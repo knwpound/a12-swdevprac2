0c181cefdb529b78511422f824fb33ba1be54921
knwpound <pound.somparn@gmail.com>
2025-10-30 16:15:43 +0700

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,45 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { bookSlice } from "./features/bookSlice";
 import { UseSelector, TypedUseSelectorHook, useSelector } from "react-redux";
+import { persistReducer,FLUSH,REHYDRATE,PAUSE,PERSIST,REGISTER, PURGE } from "redux-persist";
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+import { WebStorage } from "redux-persist/lib/types";
+
+function createPersistStorage(): WebStorage {
+  const isServer = typeof window === "undefined";
+  if (isServer) {
+    return {
+      getItem() {
+        return Promise.resolve(null);
+      },
+      setItem() {
+        return Promise.resolve();
+      },
+      removeItem() {
+        return Promise.resolve();
+      },
+    };
+  }
+  return createWebStorage('local');
+}
+
+const storage = createPersistStorage()
+
+const persistConfig = {
+  key: "rootPersist",
+  storage
+}
+
+const rootReducer = combineReducers({bookSlice: bookSlice.reducer})
+const reduxPersistedReducer = persistReducer(persistConfig,rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    bookSlice: bookSlice.reducer,
-  }
+  reducer: reduxPersistedReducer,
+  middleware:(getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck:{
+      ignoredActions:[FLUSH,REGISTER,REHYDRATE,PAUSE,PERSIST,PURGE],
+    }
+  })
 });
 
 export type RootState = ReturnType<typeof store.getState>
